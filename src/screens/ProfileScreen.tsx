268a81b22da1fb36237343f0c712880d91b05214
asha12kat-arch <asha12kat@gmail.com>
2026-09-16 00:41:44 +0100

    Update ProfileScreen.tsx

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -156,7 +156,6 @@ export default function ProfileScreen({ navigate }: Props) {
             <div className="content-stretch flex flex-col gap-[10px] items-start relative shrink-0 w-full">
               <InfoRow label="DOB" value="05/14/1984" />
               <InfoRow label="Phone" value="•••-•••-4523" />
-              <InfoRow label="Email" value="LLT7200@GMAIL.COM" />
             </div>
           </div>
 
